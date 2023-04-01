e53415b23e4672989febeec750f0f2c2dea8e1e4
Phú Thịnh <70939839+phudthinh@users.noreply.github.com>
2023-04-02 04:10:34 +0700

    Fix bug

---
+++ LIGHTEX/Models/Domain/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LIGHTEX.Models
+{
+    public class Product
+    {
+        [Key]
+        public int id_product { get; set; }
+        public int id_category { get; set; }
+        public int id_brand { get; set; }
+        public string name { get; set; }
+        public string information { get; set; }
+        public double price { get; set; }
+        public byte[] image { get; set; }
+        public bool status { get; set; }
+        public DateTime create_date { get; set; }
+        public DateTime modified_date { get; set; }
+    }
+}
